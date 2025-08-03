2726e548c69d393b70c5e9d59fc1178dbf6d9f38
MadhuMathi-7 <madhumathiravikrishnan@gmail.com>
2025-08-03 09:46:52 +0530

    Update CircularSinglyLinkedList.java

--- Data_Structure/src/linear/CircularSinglyLinkedList.java
+++ Data_Structure/src/linear/CircularSinglyLinkedList.java
@@ -1,107 +1,116 @@
 package linear;
-
-
-class CNode{
-	int data;
-	CNode next;
-	public CNode(int data) {
-		this.data=data;
-	}
-}
-class CMethods{
-	static CNode head,tail;
-	public static void insertAtBeg(int data) {
-		CNode node=new CNode(data);
-		if(head==null) {
-			head=node;
-			tail=node;
-			node.next=head;
-			return;
-		}
-		else {
-			node.next=head;
-			head=node;
-			tail.next=head;
-		}
-	}
-	public static void insertAtEnd(int data) {
-	    CNode node=new CNode(data);
-	    if (head==null) {
-	        head=node;
-	        tail=node;
-	        node.next=head;
-	        return;
-	    } else {
-	        tail.next=node;
-	        node.next=head;
-	        tail=node;
-	    }
-	}
-public static void display() {
-	CNode temp=head;
-	do {
-		System.out.print(temp.data+" ");
-		temp=temp.next;
-	}while(temp!=head);
-	System.out.println();
-}
-public static void insertAtpos(int data,int pos){
-    CNode node=new CNode(data);
-    if(pos==0){
-        insertAtBeg(data);
+class CirNode{
+    int data;
+    CirNode next;
+    public CirNode(int data) {
+        this.data=data;
+        next=null;
     }
-    else 
-    {
-        CNode temp=head;
-        for(int i=1;i<pos;i++) 
-        {
-            temp=temp.next;
+}
+class CirMethods{
+    static CirNode head,tail;
+    public static void insertAtbeg(int data) {
+        CirNode node=new CirNode(data);
+        if(head==null) {
+            head=node;
+            tail=node;
+            node.next=head;
+            return;
         }
-        node.next=temp.next;
-        temp.next=node;
-    }
-}
-public static void deleteAtBeg() {
-    CNode temp=head;
-	if(head==null && tail==null) {
-		System.out.println("List is empty");
-	}
-	else {
-		tail.next=head.next;
-		head=head.next;
-		temp.next=null;
-		
-	}
-  }
-public static void deletionAtpos(int pos) {
-    if(pos==0) {
-        deleteAtBeg();
-    }
-    else
-    {
-        CNode temp=head;
-        for(int i=1;i<pos;i++) {
-            temp=temp.next;
+        else {
+            node.next=head;
+            head=node;
+            tail.next=head;
         }
-        temp.next=temp.next.next;
+    }
+    public static void insertAtEnd(int data) {
+        CirNode node=new CirNode(data);
+        if(head==null) {
+            head=node;
+            tail=node;
+            node.next=head;
+            return;
+        }
+        else {
+            tail.next=node;
+            node.next=head;
+            tail=node;
+        }
+    }
+    public static void insertAtPos(int data,int pos){
+        CirNode newNode = new CirNode(data);
+        if (pos == 0) {
+            insertAtbeg(data);
+            return;
+        }
+        else {
+            CirNode temp = head;
+            for(int i=0; i<pos-1; i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+    public static void deleteAtBeg() {
+        CirNode temp=head;
+        if(head==null && tail==null) {
+            System.out.println("List is Empty");
+        }
+        else {
+            tail.next=head.next;
+            head=head.next;
+            temp.next=null;
+        }
+    }
+    public static void deleteAtEnd() {
+        CirNode temp=head;
+        if(head==null && tail==null) {
+            System.out.print("The list is Empty");
+        }else {
+            while(temp.next.next!=head) {
+                temp=temp.next;
+            }temp.next=head;
+            
+        }
+    }
+    public static void deleteAtPos(int pos) {
+        if(pos==0) {
+            deleteAtBeg();
+        }
+        else {
+            CirNode temp=head;
+            for(int i=1;i<pos;i++) {
+                temp=temp.next;
+            }
+            temp.next=temp.next.next;
+        }
+    }
     
+    public static void display() {
+        CirNode temp=head;
+        do {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        } while(temp!=head) ;
+        System.out.println();
     }
 }
+public class CircularSinglyLinkedList extends CirMethods {
+
+    public static void main(String[] args) {
+        insertAtbeg(5);
+        insertAtbeg(15);
+        insertAtEnd(25);
+        insertAtEnd(35);
+        insertAtPos(45,2);
+        deleteAtBeg();
+        deleteAtEnd();
+        deleteAtPos(2);
+        display();
+
+    }
+
 }
 
-public class CircularSinglyLinkedList extends CMethods {
-    public static void main(String[] args) {
-    	insertAtBeg(0);
-    	insertAtBeg(1);
-    	insertAtBeg(2);
-    	insertAtBeg(3);
-    	insertAtEnd(10);
-    	insertAtEnd(30);
-    	insertAtEnd(50);
-    	insertAtpos(300,2);
-    	deleteAtBeg();
-    	deletionAtpos(2);
-    	display();
-    }
-}
 
