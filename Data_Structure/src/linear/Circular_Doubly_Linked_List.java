3aadad70486c6f2d64a553aee07e22bc7efbe055
MadhuMathi-7 <madhumathiravikrishnan@gmail.com>
2025-08-03 10:14:12 +0530

    Update Circular_Doubly_Linked_List.java

--- Data_Structure/src/linear/Circular_Doubly_Linked_List.java
+++ Data_Structure/src/linear/Circular_Doubly_Linked_List.java
@@ -25,7 +25,60 @@ class CirDubMethods{
                 head.prev=newnode;
                 head=newnode;
             }count++;
-    }public static void display() {
+    }public static void insertAtEnd(int data) {
+        CirDubNode newnode=new CirDubNode(data);
+        if(head==null && tail==null) {
+            insertAtBeg(data);
+        }else {
+            tail.next=newnode;
+            newnode.prev=tail;
+            head.prev=newnode;
+            newnode.next=head;
+            tail=newnode;
+        }count++;
+}public static void insertAtpos(int data,int pos) {
+    CirDubNode newnode=new CirDubNode(data);
+    if(pos==0) {
+        insertAtBeg(data);
+    }
+   else {    
+        CirDubNode temp=head;
+        for(int i=1;i<pos;i++) {
+            temp=temp.next;
+        }
+        newnode.next=temp.next;
+        temp.next.prev=newnode;
+        temp.next=newnode;
+        count++;
+    }
+}public static void deletionAtBeg() {
+    if(head==null && tail==null) {
+        System.out.printf("the List is empty");
+    }else {
+        tail.next=head.next;
+        head.next.prev=tail;
+        head=head.next;
+        
+    }count--;
+}public static void deletionAtEnd() {
+    if(head==null && tail==null) {
+        System.out.printf("the List is empty");
+    }else {
+        tail.prev.next=head;
+        head.prev=tail.prev;
+        tail=tail.prev;
+    }count--;
+}public static void deletionAtpos(int pos) {
+    if(pos==0) {
+        deletionAtBeg();
+    }else{
+        CirDubNode temp=head;
+        for(int i=1;i<pos;i++) {
+            temp=temp.next;
+        }temp.next=temp.next.next;
+        count--;
+    }
+}public static void display() {
         CirDubNode temp=head;
         do {
             System.out.print(temp.data+" ");
@@ -43,9 +96,18 @@ public class Circular_Doubly_Linked_List extends CirDubMethods  {
     public static void main(String[] args) {
         insertAtBeg(7);
         insertAtBeg(8);
+        insertAtEnd(9);
+        insertAtEnd(10);
+        insertAtBeg(4);
+        insertAtpos(2,0);
+        insertAtEnd(6);
+        insertAtBeg(1);
+        deletionAtBeg();
+        deletionAtEnd();
+        deletionAtEnd();
+        deletionAtpos(1);
         display();
     
     }
 
 }
-
